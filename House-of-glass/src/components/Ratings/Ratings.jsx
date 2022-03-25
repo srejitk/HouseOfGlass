113265f1b14334c766bea85c0f942bff8743425e
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-03-26 03:58:09 +0530

    Feat : Added Ratings.jsx

---
+++ House-of-glass/src/components/Ratings/Ratings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Ratings() {
+  return (
+    <div className="rating">
+              <p className="subtitle-2 text--grey">4.2</p>
+              <span className="material-icons md-18 star_selected">star</span>
+              <span className="material-icons md-18 star_selected">star</span>
+              <span className="material-icons md-18 star_selected">star</span>
+              <span className="material-icons md-18 star_selected">star</span>
+              <span className="material-icons md-18 star_selected">star_half</span>
+            </div>)
+}

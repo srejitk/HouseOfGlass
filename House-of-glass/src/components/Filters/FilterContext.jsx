e067ebf82e03b8b64287cc53d6c406ab38c992e0
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-03-27 13:22:14 +0530

    Removed Comments

--- House-of-glass/src/components/Filters/FilterContext.jsx
+++ House-of-glass/src/components/Filters/FilterContext.jsx
@@ -5,25 +5,7 @@ import { products } from "backend/db/products";
 
 const FilterContext = createContext();
 
-
-//useContext in itself a func , and useFilter is a custom Hook so you have to assign it as a func instead of making it a variable
 const useFilter = () => useContext(FilterContext);
-
-
-// const [data, setData]=useState([]);
-
-// TO BE ADDED LATER
-//     useEffect(()=>{
-//         (async()=>{
-//             const response = await axios.get("/api/products")
-//             let {data} = response;
-//             let {products} = data;
-//             setData(products);
-//     })()
-//     },[]
-//     )
-
-
 
 const FilterProvider = ({children})=>{
     const [state,dispatch]=useReducer(FilterReducer,{sortBy:"",fastDelivery:false,outOfStock:false,categoryState:[],minPrice:12000})

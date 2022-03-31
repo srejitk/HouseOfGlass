9b0f791743534e292701a27cb89b1bfaeab35065
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-01 04:20:50 +0530

    Chore : Formatted Code

--- House-of-glass/src/Contexts/Cart/CartReducer.jsx
+++ House-of-glass/src/Contexts/Cart/CartReducer.jsx
@@ -1,19 +1,22 @@
-export const cartReducer = (state, {type,payload})=>{
-    const initialCartState = {cart:[],cartCount:0,cartSum:0}
-    switch(type){
-        case "ADD_TO_CART":
-        case "DELETE_FROM_CART":
-        case "INCREMENT_CART":
-        case "DECREMENT_CART":
-            return {...state,
-                cart:payload,
-                cartCount: payload.reduce((acc,curr)=>(acc+=curr.qty),0),
-                cartSum: payload.reduce((acc,curr)=>(acc+=curr.sellingPrice*curr.qty),0)
-
-            }
-        case "CHECKOUT":
-            return initialCartState;
-        default:
-            return state;
-    }
-}
+export const cartReducer = (state, { type, payload }) => {
+  const initialCartState = { cart: [], cartCount: 0, cartSum: 0 };
+  switch (type) {
+    case "ADD_TO_CART":
+    case "DELETE_FROM_CART":
+    case "INCREMENT_CART":
+    case "DECREMENT_CART":
+      return {
+        ...state,
+        cart: payload,
+        cartCount: payload.reduce((acc, curr) => (acc += curr.qty), 0),
+        cartSum: payload.reduce(
+          (acc, curr) => (acc += curr.sellingPrice * curr.qty),
+          0
+        ),
+      };
+    case "CHECKOUT":
+      return initialCartState;
+    default:
+      return state;
+  }
+};
--- House-of-glass/src/pages/cart/Cart.jsx
+++ House-of-glass/src/pages/cart/Cart.jsx
@@ -1,48 +1,43 @@
-import ProductList from 'components/Product List/ProductList'
-import React from 'react'
-import "./Cart.css"
-import { useAuth } from 'Utils/AuthContext'
-import WishListCard from 'components/Cards/WishListCard/WishListCard';
-import { useCart } from 'Contexts/Cart/CartContext';
+import ProductList from "components/Product List/ProductList";
+import React from "react";
+import "./Cart.css";
+import WishListCard from "components/Cards/WishListCard/WishListCard";
+import { useCart } from "Contexts/Cart/CartContext";
 
 export default function Cart() {
-  // const {userDetails}=useAuth();
-  // const {cartList} = userDetails;
-  const {cart} = useCart()
+  const { cart } = useCart();
   return (
     <main className="content flex--row--nowrap position-relative">
-  <div className="glass__container  flex--row--wrap gap20 ">
-
-  {cart.map((Product) => (<WishListCard key={Product._id} Item={Product}/>))}
-   
-    
-  </div>
-  <div className="linebreaker" />
-  <div className="glass__checkout top box-shadow flex--column--wrap gap20">
-    <h5 className="header-5">Price Breakdown</h5>
-    <div className="items--sum">
-      <p className="subtitle-1">Price ( 7 Items )</p>
-      <p className="body-1">Rs. 5,299</p>
-    </div>
-    <div className="items--discount">
-      <p className="subtitle-1">Discount</p>
-      <p className="body-1">-Rs. 299</p>
-    </div>
-    <div className="items--delivery">
-      <p className="subtitle-1">Delivery Charges</p>
-      <p className="body-1">Rs. 99</p>
-    </div>
-    <div className="items--total">
-      <p className="subtitle-1">Total</p>
-      <p className="body-1">Rs. 5,099</p>
-    </div>
-    <div className="items--savings">
-      <p className="subtitle-1">You've Saved</p>
-      <p className="body-1">Rs. 299</p>
-    </div>
-    <button className="btn btn--primary">Place Order </button>
-  </div>
-</main>
-
-  )
+      <div className="glass__container  flex--row--wrap gap20 ">
+        {cart.map((Product) => (
+          <WishListCard key={Product._id} Item={Product} />
+        ))}
+      </div>
+      <div className="linebreaker" />
+      <div className="glass__checkout top box-shadow flex--column--wrap gap20">
+        <h5 className="header-5">Price Breakdown</h5>
+        <div className="items--sum">
+          <p className="subtitle-1">Price ( 7 Items )</p>
+          <p className="body-1">Rs. 5,299</p>
+        </div>
+        <div className="items--discount">
+          <p className="subtitle-1">Discount</p>
+          <p className="body-1">-Rs. 299</p>
+        </div>
+        <div className="items--delivery">
+          <p className="subtitle-1">Delivery Charges</p>
+          <p className="body-1">Rs. 99</p>
+        </div>
+        <div className="items--total">
+          <p className="subtitle-1">Total</p>
+          <p className="body-1">Rs. 5,099</p>
+        </div>
+        <div className="items--savings">
+          <p className="subtitle-1">You've Saved</p>
+          <p className="body-1">Rs. 299</p>
+        </div>
+        <button className="btn btn--primary">Place Order </button>
+      </div>
+    </main>
+  );
 }

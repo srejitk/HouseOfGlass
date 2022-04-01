5c915e9facf794532af6b84aabb13e3012cabfef
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-02 00:11:06 +0530

    Feat : Added Category Filtering

--- House-of-glass/src/backend/db/categories.js
+++ House-of-glass/src/backend/db/categories.js
@@ -8,19 +8,71 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "ROUND",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648061613/House%20Of%20Glass/1_eestfh.jpg",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "RETRO",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648061847/House%20Of%20Glass/2_urb8za.jpg",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "TRANSPARENT",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648061847/House%20Of%20Glass/3_cfr4hd.jpg",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "SUNGLASSES",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648061847/House%20Of%20Glass/4_bpuxos.jpg",
+    description:
+      "literature in the form of prose, especially novels, that describes imaginary events and people",
+  },
+  {
+    _id: uuid(),
+    categoryName: "CATEYE",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648061847/House%20Of%20Glass/5_cknv6i.jpg",
+    description:
+      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "RIMLESS",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648061847/House%20Of%20Glass/6_pkcskv.jpg",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "VINCENTCHASE",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648066517/House%20Of%20Glass/6_rwso2c.jpg",
+    description:
+      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "JOHNJACOBS",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648066517/House%20Of%20Glass/7_hvctpt.jpg",
+    description:
+      "literature in the form of prose, especially novels, that describes imaginary events and people",
+  },
+  {
+    _id: uuid(),
+    categoryName: "RAYBAN",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648066517/House%20Of%20Glass/8_sjopta.jpg",
+    description:
+      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "VOGUE",
+    img: "https://res.cloudinary.com/dkqrmlxlg/image/upload/v1648066517/House%20Of%20Glass/9_owecdk.jpg",
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
   },

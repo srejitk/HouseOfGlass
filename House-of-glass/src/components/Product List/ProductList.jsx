a34f5c1e5e0113165a7149c52fd25861600f0ef8
GenesisAlpha <kumar.edwardcullen.sreejith80@gmail.com>
2022-04-01 16:25:34 +0530

    Chore : Moved Product Context to Context Folder

--- House-of-glass/src/components/Product List/ProductList.jsx
+++ House-of-glass/src/components/Product List/ProductList.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import Filters from "components/Filters/Filters";
-import { useProduct } from "components/Filters/ProductContext";
+import { useProduct } from "Contexts/Product/ProductContext";
 import ProductCard from "components/Cards/Vertical/ProductCard";
 
 export default function ProductList() {
-  const { products } = useProduct();
+  const { finalProductList } = useProduct();
 
   return (
     <div className="content flex--row--wrap products-container">
       <Filters />
       <div className="products-text-container">
         <h4 className="header-4">Products</h4>
-        <h6 className="subtitle-1">Products found : {products?.length} </h6>
+        <h6 className="subtitle-1">
+          Products found : {finalProductList.length}{" "}
+        </h6>
       </div>
       <div className="products-list-container flex--row--wrap">
-        {products.map((item) => (
+        {finalProductList.map((item) => (
           <ProductCard key={item._id} Item={item} />
         ))}
       </div>

59f873e0e997e57e511fe38846cfd5e69b5789f5
Sreejith K <srejitk@gmail.com>
2023-11-28 01:36:51 +0530

    Chore : Code cleanup

--- glasshouse/src/app/page.tsx
+++ glasshouse/src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <p>GLASSHOUSE</p>;
+  return (
+    <main className="">
+      <p className="text-9xl font-semibold text-transparent uppercase bg-gradient-to-br from-slate-50 via-blue-500 via-50% to-80% to-purple-800 text-clip bg-clip-text">
+        Glasshouse
+      </p>{" "}
+    </main>
+  );
 }
